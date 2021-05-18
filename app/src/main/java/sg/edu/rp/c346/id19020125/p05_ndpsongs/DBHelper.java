0965c96acba0f6bbf54d3a927a40cc36951ea226
ys2409 <19022397@myrp.edu.sg>
2021-05-18 15:33:26 +0800

    lesson4

---
+++ app/src/main/java/sg/edu/rp/c346/id19020125/p05_ndpsongs/DBHelper.java
@@ -0,0 +1,104 @@
+package sg.edu.rp.c346.id19020125.p05_ndpsongs;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "song.db";
+    private static final int DATABASE_VER = 1;
+    private static final String TABLE_SONG = "song";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_SINGER = "singer";
+    private static final String COLUMN_YEAR = "year";
+    private static final String COLUMN_STAR = "star";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VER);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String createSongTableSql = "CREATE TABLE " + TABLE_SONG + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_TITLE + " TEXT," + COLUMN_SINGER + " TEXT,"
+                + COLUMN_YEAR + "INTEGER," + COLUMN_STAR + "INTEGER ) ";
+        sqLiteDatabase.execSQL(createSongTableSql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("ALTER TABLE " + TABLE_SONG + " ADD COLUMN song_title TEXT ");
+    }
+
+    public int updateNote(Song data){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TITLE, data.getTitle());
+        values.put(COLUMN_SINGER, data.getSinger());
+        values.put(COLUMN_YEAR, data.getYear());
+        values.put(COLUMN_STAR, data.getStar());
+        String condition = COLUMN_ID + "= ?";
+        String[] args = {String.valueOf(data.getId())};
+        int result = db.update(TABLE_SONG, values, condition, args);
+        db.close();
+        return result;
+    }
+
+    public int deleteNote(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String condition = COLUMN_ID + "= ?";
+        String[] args = {String.valueOf(id)};
+        int result = db.delete(TABLE_SONG, condition, args);
+        db.close();
+        return result;
+    }
+
+    public long insertNote (String songContent, String singer,int year, int stars){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_TITLE, songContent);
+        values.put(COLUMN_SINGER, singer);
+        values.put(COLUMN_YEAR, year);
+        values.put(COLUMN_STAR, stars);
+        long result = db.insert(TABLE_SONG, null, values);
+        db.close();
+        Log.d("SQL Insert", "ID:" + result); //id returned, shouldn’t be -1
+
+        return result;
+    }
+
+    public ArrayList<Song> getAllSongs(String keyword) {
+        ArrayList<Song> songs = new ArrayList<Song>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns= {COLUMN_ID, COLUMN_TITLE, COLUMN_SINGER, COLUMN_YEAR, COLUMN_STAR};
+        String condition = COLUMN_TITLE + " Like ?";
+        String[] args = { "%" +  keyword + "%"};
+        Cursor cursor = db.query(TABLE_SONG, columns, condition, args,
+                null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String sTitle = cursor.getString(1);
+                String sSinger = cursor.getString(1);
+                int sYear = cursor.getInt(1);
+                int star = cursor.getInt(1);
+                Song song = new Song(sTitle, sSinger, sYear, star);
+                songs.add(song);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return songs;
+
+    }
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id19020125/p05_ndpsongs/Song.java
@@ -0,0 +1,54 @@
+package sg.edu.rp.c346.id19020125.p05_ndpsongs;
+
+import java.io.Serializable;
+
+public class Song implements Serializable {
+    private int id;
+    private String singer;
+    private String title;
+    private int year;
+    private int star;
+
+    public Song(String singer, String title, int year, int star){
+        this.singer = singer;
+        this.title = title;
+        this.year = year;
+        this.star = star;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setStar(int star) {
+        this.star = star;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}

d86ee171e0d5a70f48ae6a35f75096fb31a60f1d
19020125 <19020125@myrp.edu.sg>
2021-05-18 15:00:37 +0800

    Done activity_main.xml and initialize the variables with ui.

--- app/src/main/java/sg/edu/rp/c346/id19020125/p05_ndpsongs/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19020125/p05_ndpsongs/MainActivity.java
@@ -3,12 +3,27 @@ package sg.edu.rp.c346.id19020125.p05_ndpsongs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etSongTitle, etSinger, etYear;
+    RadioGroup rg;
+    Button btnInsert, btnShowList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etSongTitle = findViewById(R.id.etSongTitle);
+        etSinger = findViewById(R.id.etSingerName);
+        etYear = findViewById(R.id.etYear);
+        btnInsert = findViewById(R.id.btnInsert);
+        btnShowList = findViewById(R.id.btnShowList);
+
+
     }
 }

deb0477850dea62c2a86c8f7148e84827ee9ef50
19020125 <19020125@myrp.edu.sg>
2021-05-18 15:17:38 +0800

    Done intent to second activity, updated AndroidManifest.xml and retrieved inputs from edit text.

--- app/src/main/java/sg/edu/rp/c346/id19020125/p05_ndpsongs/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19020125/p05_ndpsongs/MainActivity.java
@@ -2,7 +2,9 @@ package sg.edu.rp.c346.id19020125.p05_ndpsongs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
@@ -21,9 +23,26 @@ public class MainActivity extends AppCompatActivity {
         etSongTitle = findViewById(R.id.etSongTitle);
         etSinger = findViewById(R.id.etSingerName);
         etYear = findViewById(R.id.etYear);
+        rg = findViewById(R.id.rg);
         btnInsert = findViewById(R.id.btnInsert);
         btnShowList = findViewById(R.id.btnShowList);
 
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputSongTitle = etSongTitle.getText().toString();
+                String inputSinger = etSinger.getText().toString();
+                String inputYear = etYear.getText().toString();
+                int SelectedId = rg.getCheckedRadioButtonId();
+            }
+        });
 
+        btnShowList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                startActivityForResult(i, 10);
+            }
+        });
     }
 }
---
+++ app/src/main/java/sg/edu/rp/c346/id19020125/p05_ndpsongs/SecondActivity.java
@@ -0,0 +1,13 @@
+package sg.edu.rp.c346.id19020125.p05_ndpsongs;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}
